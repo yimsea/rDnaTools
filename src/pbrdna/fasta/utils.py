66a9f35fa06afd49dd2e18902a79f33a8170c334
Brett Bowman <bbowman@pacificbiosciences.com>
2013-07-01 15:05:09 -0700

    Add missing library files

---
+++ src/pbrdna/fasta/utils.py
@@ -0,0 +1,15 @@
+from pbrdna.io.FastaIO import FastaReader
+
+def fasta_count( fasta_file ):
+    count = 0
+    try:
+        for record in FastaReader( fasta_file ):
+            if len(record.sequence) > 0:
+                count += 1
+    except:
+        pass
+    return count
+
+def copy_fasta_sequences( fasta_file, fasta_writer ):
+    for fasta_record in FastaReader( fasta_file ):
+        fasta_writer.writeRecord( fasta_record )
