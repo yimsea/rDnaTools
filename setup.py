3176d68ec7a38a77a14c7e237da18a26242ee6f5
Brett Bowman <bbowman@pacificbiosciences.com>
2014-02-17 17:32:06 -0800

    Bumpped the minor version number for recent changes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if ("install" in sys.argv) and sys.version_info < (2, 7, 0):
 
 setup(
     name = 'rDnaTools',
-    version='0.1.2',
+    version='0.1.3',
     author='Brett Bowman',
     author_email='bbowman@pacificbiosciences.com',
     url='https://github.com/bnbowman/rDnaTools',
